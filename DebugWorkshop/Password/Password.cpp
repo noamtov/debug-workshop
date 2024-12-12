61ecae33a6ef2eebee896739fc721ab78192ca92
unknown <noam4632@gmail.com>
2024-12-12 21:25:20 +0200

    submission the workshop

---
+++ DebugWorkshop/Password/Password.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+struct Password
+{
+	char value[16];
+	bool incorrect;
+	Password() : value(""), incorrect(true)
+	{
+	}
+};
+
+int main()
+{
+	std::cout << "Enter your password to continue:" << std::endl;
+	Password pwd;
+	std::cin >> pwd.value;
+
+	if (!strcmp(pwd.value, "********"))
+		pwd.incorrect = false;
+
+	if(!pwd.incorrect)
+		std::cout << "Congratulations\n";
+
+	return 0;
+}
+//יכולות להיות סיסמאות שיותר ארוכות מ15 תווים שיעבדו
+//למשל הסיסמא abcdefghijklmnop אמורה לא לעבוד אך היא תעבוד
